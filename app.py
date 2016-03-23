006b9cc03b12465c9800eb00e1982cbccf0a6f08
David Axelrod <daxaxelrod@gmail.com>
2016-03-23 00:33:10 -0400

    adding port specifications for heroku

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +7,5 @@ def index():
     return "this is a test for gcloud. lets see if we can get this up and running"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
