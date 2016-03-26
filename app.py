4684c992b922995156736003e98b665338704767
David Axelrod <daxaxelrod@gmail.com>
2016-03-26 00:33:30 -0400

    login logic done. now to make it pretty

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
-from flask import Flask, render_template,g, redirect
+from flask import Flask, render_template,g, redirect, url_for, flash
 from flask.ext.bcrypt import check_password_hash
 from flask.ext.login import (LoginManager, login_user, logout_user, login_required, current_user)
 
 import forms
+import models
 
 app = Flask(__name__)
 
@@ -13,6 +14,19 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
+
+@app.before_request
+def before_request():
+    g.db = models.DATABASE
+    g.db.connect()
+    g.user = current_user
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -21,17 +35,43 @@ def index():
 def login():
     form = forms.LoginForm()
     if form.validate_on_submit():
-        return "awesome login bro, welocme to loom <a href="">Go Home</a>"
+        try:
+            loom_user = models.User.get(models.User.email == form.email.data)
+        except models.DoesNotExist:
+            flash("Email is incorrect. If you havent registered before, <a href='/register'>Go here!</a>", "error")
+        else:
+            if check_password_hash(loom_user.password, form.password.data):
+                login_user(loom_user) # could make an extra field remember me as a boolean
+                flash("Logged in", "success")
+                return redirect(url_for('index'))
+            else:
+                flash("Password doesnt match our records", "error")
+
     return render_template("login.html", form=form)
+
+@app.route("/logout", methods=("GET", "POST"))
+def logout():
+    logout_user()
+    flash("See you soon!", "success")
+    return redirect(url_for('index'))
 
 @app.route("/register", methods=("GET", "POST"))
 def register():
     form = forms.RegisterForm()
     if form.validate_on_submit():
-        return "nice register form submission <a href="">Go Home</a>"
+        models.User.create_user(form.username.data,form.firstname.data,
+                                            form.lastname.data,form.email.data, form.password.data)
+        new_loomer = models.User.select().where(models.User.email ==form.email.data).get()
+        login_user(new_loomer)
+        return redirect(url_for("index"))
+        # return "nice register form submission <a href="">Go Home</a>"
     return render_template("register.html", form=form)
 
+@app.route("/blog")
+def blog():
+    return "Blog coming in the future"
 
 if __name__ == "__main__":
+    models.initialize()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
--- models.py
+++ models.py
@@ -1,3 +1,45 @@
 # # models work
 # basic user info
 # whether or not they paid
+import datetime
+from flask import flash
+from peewee import *
+from flask.ext.bcrypt import generate_password_hash
+from flask.ext.login import UserMixin
+
+DATABASE = SqliteDatabase('loomy.db')
+
+class User(Model, UserMixin):
+
+    class Meta:
+        datebase = DATABASE
+        order_by = ('-joined_at',)
+
+    username = CharField(unique=True)
+    first_name = CharField()
+    last_name = CharField()
+    email = CharField(unique=True) # validation is handled through forms
+    password = CharField(max_length=120)
+    joined_at = DateTimeField(default=datetime.datetime.now)
+    is_admin= BooleanField(default=False)
+    testimonial = TextField(null=True)
+
+    @classmethod
+    def create_user(cls, username, first_name, last_name, email, passwordinput, is_admin=False):
+        try:
+            with DATABASE.transaction():
+                cls.create(
+                #order matters
+                username = username,
+                first_name = first_name,
+                last_name = last_name,
+                email = email,
+                password = generate_password_hash(passwordinput),
+                is_admin = is_admin
+                )
+        except IntegrityError:
+            flash("We already have a user like that. Try logging in!")
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User], safe=True)
+    DATABASE.close()
